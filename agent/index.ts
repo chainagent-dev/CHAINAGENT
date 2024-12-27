4385e7fa1ce5d4739e9537f125cb650d958c2ed8
chainagent-dev <chainagentai@gmail.com>
2024-12-26 18:50:45 -0800

    Update index.ts

--- agent/index.ts
+++ agent/index.ts
@@ -2,6 +2,7 @@ import { SolanaAgentKit } from "./agent";
 import { createSolanaTools } from "./langchain";
 
 export { SolanaAgentKit, createSolanaTools };
+export { ChainAgent, ChainAgentTools };
 
 // Optional: Export types that users might need
 export * from "./types";
